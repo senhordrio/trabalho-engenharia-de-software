85717df36305f8ca99dd046cfec28753d94ffc80
senhordrio <adriosilvao@gmail.com>
2020-08-12 21:58:30 -0300

    realizando imports

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,3 +1,6 @@
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
 require('./bootstrap');
 window.Vue = require('vue');
 
@@ -6,9 +9,13 @@ import VueRouter from 'vue-router';
 import VueAxios from 'vue-axios';
 import axios from 'axios';
 import {routes} from './routes';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(VueRouter);
 Vue.use(VueAxios, axios);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 const router = new VueRouter({
     mode: 'history',

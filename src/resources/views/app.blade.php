b4836d7f9ece446c0fa7ab31085c0e326293e4f3
senhordrio <adriosilvao@gmail.com>
2020-08-12 21:52:04 -0300

    Atualização de title

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" value="{{ csrf_token() }}" />
-    <title>Laravel & Vue CRUD Single Page Application (SPA) Tutorial - MyNotePaper</title>
+    <title>D&D Sistema</title>
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
     <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet" />
     <style>

363a35ddab4bcf9e067c7d4ba95d8395f6cf0e5b
ianbertolucci <ian.diasbertolucci@gmail.com>
2020-08-22 19:07:31 -0300

    Conflitos de merge

--- src/resources/js/routes.js
+++ src/resources/js/routes.js
@@ -1,14 +1,14 @@
-
+import Dashboard from "./components/pages/Dashboard.vue";
 import ListaTransacoes from "./components/pages/ListaTransacoes.vue";
 import AdicionarTransacao from "./components/pages/AdicionarTransacao.vue";
+import EditarTransacao from "./components/pages/EditarTransacao.vue";
 import ListaProdutos from "./components/pages/ListaProdutos.vue";
 import AdicionarProduto from "./components/pages/AdicionarProduto.vue";
 import EditarProduto from "./components/pages/EditarProduto.vue";
-import Dashboard from "./components/pages/Dashboard.vue";
 import ListaEmpresas from "./components/pages/ListaEmpresas";
 import AdicionarEmpresa from "./components/pages/AdicionarEmpresa";
+import EditarEmpresa from "./components/pages/EditarEmpresa";
 import Login from "./components/pages/Login";
-
 
 export const routes = [
     {
@@ -47,6 +47,11 @@ export const routes = [
         component: AdicionarTransacao
     },
     {
+        name: "detalhesTransacao",
+        path: "/editarTransacao",
+        component: EditarTransacao
+    },
+    {
         name: "lista-empresas",
         path: "/lista-empresas",
         component: ListaEmpresas
@@ -55,5 +60,10 @@ export const routes = [
         name: "adicionarEmpresa",
         path: "/adicionarEmpresa",
         component: AdicionarEmpresa
-    }
+    },
+    {
+        name: "detalhesEmpresa",
+        path: "/editarEmpresa",
+        component: EditarEmpresa
+    },
 ];
